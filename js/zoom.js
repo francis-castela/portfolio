8df9a1d5671f04b4dd841aa7f9d763b9f48d276b
francis-castela <franciscastela.42@gmail.com>
2025-04-05 15:22:54 -0300

    1

---
+++ js/zoom.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('img').forEach(img => {
+    img.addEventListener('click', () => {
+        const modal = document.getElementById('imageModal');
+        const modalImg = document.getElementById('modalImage');
+        modal.style.display = 'flex';
+        modalImg.src = img.src;
+    });
+});
+
+document.querySelector('.close-modal').addEventListener('click', () => {
+    document.getElementById('imageModal').style.display = 'none';
+});
+
+document.getElementById('imageModal').addEventListener('click', (e) => {
+    if(e.target === document.getElementById('imageModal')) {
+        document.getElementById('imageModal').style.display = 'none';
+    }
+});
